8ac9aba4fd25d58fffde176b9030e935b70be14c
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-02-18 18:33:32 -0300

    refactor: remove console.log

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,10 +5,8 @@ const tempDB = [];
 
 routes.post("/book", (req, res) => {
   const { title, author, publicationYear } = req.body;
-
   tempDB.push({ title, author, publicationYear });
 
-  console.log(tempDB[0]);
   const myRes = tempDB[0];
   res.json({
     newBook: myRes,
