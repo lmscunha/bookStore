ee7b8474f4ab4c80413162631e9de0f8e2c143af
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-02-25 17:28:28 -0300

    feat: add id to books - upate list route - create get a book route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,4 @@
+const { v4: uuidv4 } = require("uuid");
 const { Router } = require("express");
 
 const routes = Router();
@@ -5,18 +6,29 @@ const tempDB = [];
 
 routes.post("/book", (req, res) => {
   const { title, author, publicationYear } = req.body;
-  tempDB.push({ title, author, publicationYear });
+  const newBook = {
+    id: uuidv4(),
+    title,
+    author,
+    publicationYear,
+  };
 
-  const myRes = tempDB[0];
-  res.json({
-    newBook: myRes,
-  });
+  tempDB.push(newBook);
+
+  res.json(newBook);
 });
 
 routes.get("/book", (req, res) => {
   res.json({
     book_list: tempDB,
   });
+});
+
+routes.get("/book/:id", (req, res) => {
+  const bookId = req.params.id;
+  const book = tempDB.filter((book) => book.id === bookId);
+
+  res.json(book[0]);
 });
 
 module.exports = routes;
