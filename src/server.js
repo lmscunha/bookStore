a56ad729e5c282ebb202833120a0eb67b376ac94
Leonardo Cunha <agile.ice4816@fastmail.com>
2023-03-12 21:47:00 -0300

    refactor: create a middleware file

--- src/server.js
+++ src/server.js
@@ -4,26 +4,6 @@ const routes = require("./routes/routes");
 const app = express();
 app.use(express.json());
 
-const bookData = function (req, res, next) {
-  const { title, author, publicationYear } = req.body;
-
-  const parsedTitle = title;
-  const parsedAuthor = author;
-  const parsedPY = +publicationYear;
-  const validBookData =
-    typeof parsedTitle === "string" &&
-    typeof parsedAuthor === "string" &&
-    parsedPY > 0;
-
-  if (!validBookData) {
-    return res.status(403).json({ message: "Invalid book data" });
-  }
-
-  next();
-};
-
-app.use(bookData);
-
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
